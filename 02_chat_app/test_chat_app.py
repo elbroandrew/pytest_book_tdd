acbc8b2ba58c7c0b6268fd5b14e772229377fa34
Andrew <elbroandrew@gmail.com>
2023-06-26 09:12:41 +1000

    using spy for connection

--- 02_chat_app/test_chat_app.py
+++ 02_chat_app/test_chat_app.py
@@ -25,6 +25,15 @@ class TestChatClient(unittest.TestCase):
         sent_message = client.send_message("Hello World")
         assert sent_message == "User 1: Hello World"
 
+    def test_client_connection(self):
+        client = ChatClient("User 1")
+
+        connection_spy = unittest.mock.MagicMock()
+        with unittest.mock.patch.object(client, "_get_connection", return_value=connection_spy):
+            client.send_message("Hello World")
+        # assert that the spy was called with the expected data to broadcast
+        connection_spy.broadcast.assert_called_with(("User 1: Hello World"))
+
 
 class TestConnection(unittest.TestCase):
     def test_broadcast(self):
@@ -46,11 +55,27 @@ class ChatClient:
 
     def __init__(self, nickname):
         self.nickname = nickname
+        self._connection = None
 
     def send_message(self, message):
         sent_message = "{}: {}".format(self.nickname, message)
-        self.connection.broadcast(message)
+        self.connection.broadcast(sent_message)
         return sent_message
+
+    @property
+    def connection(self):
+        if self._connection is None:
+            self._connection = self._get_connection()
+        return self._connection
+
+    @connection.setter
+    def connection(self, value):
+        if self._connection is not None:
+            self._connection.close()
+        self._connection = value
+
+    def _get_connection(self):
+        return Connection(("localhost", 9090))
 
 
 """
