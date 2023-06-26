079d334ca3427e33dc890c47b2dedcc8d3d48aa7
Andrew <elbroandrew@gmail.com>
2023-06-26 22:18:25 +1000

    accept commands util quit

--- 03_Todo_List_TDD/src/todo/app.py
+++ 03_Todo_List_TDD/src/todo/app.py
@@ -1,4 +1,20 @@
-
+import functools
 
 class TodoApp:
-    pass
+    def __init__(self, io=(input, functools.partial(print, end=""))):  # use builtin input and print to input and output
+        self._in, self._out = io
+        self._quit = False
+
+    def run(self):
+        self._quit = False
+        while not self._quit:
+            self._out(self.prompt(""))
+            command = self._in()
+        self._out("bye!\n")
+
+    def prompt(self, output):
+        return """TODOs:
+{}
+
+> """.format(output)
+
