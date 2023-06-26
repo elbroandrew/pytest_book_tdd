8afbbf78d117be7577382bf52c32efecb9c26ab5
Andrew <elbroandrew@gmail.com>
2023-06-26 21:25:03 +1000

    create todo template

--- 03_Todo_List_TDD/tests/test_acceptance.py
+++ 03_Todo_List_TDD/tests/test_acceptance.py
@@ -1,6 +1,23 @@
 import unittest
+import threading  # to run app on the background during our test
 
 
 class TestTodoAcceptance(unittest.TestCase):
     def test_main(self):
-        raise NotImplementedError()
+        app = TodoApp(io=(self.fake_input, self.fake_output))
+        app_thread = threading.Thread(target=app.run, daemon=True)
+        app_thread.start()
+
+        welcome = self.get_output()
+        self.assertEqual(welcome, (
+            "TODOs:\n"
+            "\n"
+            "\n"
+            "> "
+        ))
+
+        self.send_input("quit")  # app exit on quit request
+        app_thread.join(timeout=1)
+        self.assertEqual(self.get_output(), "bye!\n")
+
+
