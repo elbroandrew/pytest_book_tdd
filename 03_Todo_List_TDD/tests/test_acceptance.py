26b5759403357ee64c2e02c2072fa8c192e76ece
Andrew <elbroandrew@gmail.com>
2023-06-26 21:51:49 +1000

    add todos

--- 03_Todo_List_TDD/tests/test_acceptance.py
+++ 03_Todo_List_TDD/tests/test_acceptance.py
@@ -15,6 +15,31 @@ class TestTodoAcceptance(unittest.TestCase):
             "\n"
             "> "
         ))
+        self.send_input("add buy milk")
+        welcome = self.get_output()
+        self.assertEqual(welcome, (
+            "TODOs:\n"
+            "1. buy milk\n"
+            "\n"
+            "> "
+        ))
+        self.send_input("add buy eggs")
+        welcome = self.get_output()
+        self.assertEqual(welcome, (
+            "TODOs:\n"
+            "1. buy milk\n"
+            "2. buy eggs\n"
+            "\n"
+            "> "
+        ))
+        self.send_input("del 1")
+        welcome = self.get_output()
+        self.assertEqual(welcome, (
+            "TODOs:\n"
+            "1. buy eggs\n"
+            "\n"
+            "> "
+        ))
 
         self.send_input("quit")  # app exit on quit request
         app_thread.join(timeout=1)
