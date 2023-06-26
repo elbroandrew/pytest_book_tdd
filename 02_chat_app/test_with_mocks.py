f1c85b09067864a921a80c0901ab46262a72a9e8
Andrew <elbroandrew@gmail.com>
2023-06-26 11:12:10 +1000

    using mock

---
+++ 02_chat_app/test_with_mocks.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import Mock
+
+"""
+mock - это пустой объект, которому так же можно создать методы и тд.
+"""
+
+
+# ф-ия, которую необходимо протестить
+def read_file(f):
+    print("\nREADING THE FILE")
+    return f.read()
+
+
+class TestReadFile(unittest.TestCase):
+
+    def test_read_file(self):
+        m = Mock()
+        read_file(m)
+
+
+
+
+
+
